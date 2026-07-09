5af9bacb8e002698af627580b613d681ccc3e834
M8700ani <manishavalavala@gmail.com>
2026-07-09 14:50:28 +0530

    Added DemoLogicApp MVC Project

---
+++ DemoLogicApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DemoLogicApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
